1f7f6b5b1b7f3b2396d958473b83269a198b1322
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2025-02-06 00:10:49 +0100

    update

--- vtu_django/settings.py
+++ vtu_django/settings.py
@@ -106,7 +106,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mederhoo@gmail.com'
-EMAIL_HOST_PASSWORD = '@Hammed16g'
+EMAIL_HOST_PASSWORD = 'password'
 
 
 
