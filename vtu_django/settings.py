27fc63450e34c8d19aacc97652d30ee60c938192
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2025-02-15 09:57:44 +0100

    db update

--- vtu_django/settings.py
+++ vtu_django/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%*cx@ey@v_%jjcu(4()pbe@bseon6ie8^as@$z$6c8jr&h#ap'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
